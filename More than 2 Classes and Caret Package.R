2ca67c2fbd3254d97b5bfd87d92b19ad933524ff
Veronicaots <79258965+Veronicaots@users.noreply.github.com>
2021-11-19 15:01:17 +0000

    Add files via upload

---
+++ More than 2 Classes and Caret Package.R
@@ -0,0 +1,303 @@
+library(tidyverse)
+library(dslabs)
+
+if(!exists("mnist")) mnist <- read_mnist()
+set.seed(0) # if using R 3.5 or earlier
+set.seed(0, sample.kind = "Rounding") # if using R 3.6 or later
+ind <- which(mnist$train$labels %in% c(2,7)) %>% sample(500)
+
+#the predictors are in x and the labels in y
+x <- mnist$train$images[ind,]
+y <- mnist$train$labels[ind]
+
+y[1:3]
+
+x_1 <- x[1,]
+x_2 <- x[2,]
+x_3 <- x[3,]
+
+#distance between two numbers
+sqrt(sum((x_1 - x_2)^2))
+sqrt(sum((x_1 - x_3)^2))
+sqrt(sum((x_2 - x_3)^2))
+
+#compute distance using matrix algebra
+sqrt(crossprod(x_1 - x_2))
+sqrt(crossprod(x_1 - x_3))
+sqrt(crossprod(x_2 - x_3))
+
+#compute distance between each row
+d <- dist(x)
+class(d)
+as.matrix(d)[1:3,1:3]
+
+#visualize these distances
+image(as.matrix(d))
+
+data("olive")
+olive %>% as_tibble()
+table(olive$region)
+olive <- select(olive, -area)
+
+# Predict region using KNN
+library(caret)
+fit <- train(region ~ .,  method = "knn", 
+             tuneGrid = data.frame(k = seq(1, 15, 2)), 
+             data = olive)
+ggplot(fit)
+
+# Plot distribution of each predictor stratified by region
+olive %>% gather(fatty_acid, percentage, -region) %>%
+  ggplot(aes(region, percentage, fill = region)) +
+  geom_boxplot() +
+  facet_wrap(~fatty_acid, scales = "free") +
+  theme(axis.text.x = element_blank())
+
+# plot values for eicosenoic and linoleic
+p <- olive %>% 
+  ggplot(aes(eicosenoic, linoleic, color = region)) + 
+  geom_point()
+p + geom_vline(xintercept = 0.065, lty = 2) + 
+  geom_segment(x = -0.2, y = 10.54, xend = 0.065, yend = 10.54, color = "black", lty = 2)
+
+# load data for regression tree
+data("polls_2008")
+qplot(day, margin, data = polls_2008)
+
+library(rpart)
+fit <- rpart(margin ~ ., data = polls_2008)
+
+# visualize the splits 
+plot(fit, margin = 0.1)
+text(fit, cex = 0.75)
+polls_2008 %>% 
+  mutate(y_hat = predict(fit)) %>% 
+  ggplot() +
+  geom_point(aes(day, margin)) +
+  geom_step(aes(day, y_hat), col="red")
+
+# change parameters
+fit <- rpart(margin ~ ., data = polls_2008, control = rpart.control(cp = 0, minsplit = 2))
+polls_2008 %>% 
+  mutate(y_hat = predict(fit)) %>% 
+  ggplot() +
+  geom_point(aes(day, margin)) +
+  geom_step(aes(day, y_hat), col="red")
+
+# use cross validation to choose cp
+library(caret)
+train_rpart <- train(margin ~ ., method = "rpart", tuneGrid = data.frame(cp = seq(0, 0.05, len = 25)), data = polls_2008)
+ggplot(train_rpart)
+
+# access the final model and plot it
+plot(train_rpart$finalModel, margin = 0.1)
+text(train_rpart$finalModel, cex = 0.75)
+polls_2008 %>% 
+  mutate(y_hat = predict(train_rpart)) %>% 
+  ggplot() +
+  geom_point(aes(day, margin)) +
+  geom_step(aes(day, y_hat), col="red")
+
+# prune the tree 
+pruned_fit <- prune(fit, cp = 0.01)
+
+# fit a classification tree and plot it
+train_rpart <- train(y ~ .,
+                     method = "rpart",
+                     tuneGrid = data.frame(cp = seq(0.0, 0.1, len = 25)),
+                     data = mnist_27$train)
+plot(train_rpart)
+
+
+# compute accuracy
+confusionMatrix(predict(train_rpart, mnist_27$test), mnist_27$test$y)$overall["Accuracy"]
+
+library(randomForest)
+fit <- randomForest(margin~., data = polls_2008) 
+plot(fit)
+
+install.packages("Rborist")
+library(Rborist)
+
+# use cross validation to choose parameter
+train_rf_2 <- train(y ~ .,
+                    method = "Rborist",
+                    tuneGrid = data.frame(predFixed = 2, minNode = c(3, 50)),
+                    data = mnist_27$train)
+confusionMatrix(predict(train_rf_2, mnist_27$test), mnist_27$test$y)$overall["Accuracy"]
+
+
+#order the distance by labels
+image(as.matrix(d)[order(y), order(y)])
+
+#compute distance between predictors
+d <- dist(t(x))
+dim(as.matrix(d))
+
+d_492 <- as.matrix(d)[492,]
+
+image(1:28, 1:28, matrix(d_492, 28, 28))
+
+
+
+## COMPREHENSION CHECK
+## COMPREHENSION CHECK
+library(rpart)
+n <- 1000
+sigma <- 0.25
+# set.seed(1) # if using R 3.5 or ealier
+set.seed(1, sample.kind = "Rounding") # if using R 3.6 or later
+x <- rnorm(n, 0, 1)
+y <- 0.75 * x + rnorm(n, 0, sigma)
+dat <- data.frame(x = x, y = y)
+
+fit <- rpart(y ~ ., data = dat)
+
+fit <- rpart(y ~ ., data = dat)
+plot(fit)
+text(fit)
+
+dat %>% 
+  mutate(y_hat = predict(fit)) %>% 
+  ggplot() +
+  geom_point(aes(x, y)) +
+  geom_step(aes(x, y_hat), col=2)
+
+library(randomForest)
+fit <- randomForest(y ~ x, data = dat) 
+dat %>% 
+  mutate(y_hat = predict(fit)) %>% 
+  ggplot() +
+  geom_point(aes(x, y)) +
+  geom_step(aes(x, y_hat), col = "red")
+
+plot(fit)
+
+library(randomForest)
+fit <- randomForest(y ~ x, data = dat, nodesize = 50, maxnodes = 25)
+dat %>% 
+  mutate(y_hat = predict(fit)) %>% 
+  ggplot() +
+  geom_point(aes(x, y)) +
+  geom_step(aes(x, y_hat), col = "red")
+
+library(tidyverse)
+library(dslabs)
+data("mnist_27")
+
+library(caret)
+train_glm <- train(y ~ ., method = "glm", data = mnist_27$train)
+train_knn <- train(y ~ ., method = "knn", data = mnist_27$train)
+
+y_hat_glm <- predict(train_glm, mnist_27$test, type = "raw")
+y_hat_knn <- predict(train_knn, mnist_27$test, type = "raw")
+
+confusionMatrix(y_hat_glm, mnist_27$test$y)$overall[["Accuracy"]]
+confusionMatrix(y_hat_knn, mnist_27$test$y)$overall[["Accuracy"]]
+
+getModelInfo("knn")
+modelLookup("knn")
+
+train_knn <- train(y ~ ., method = "knn", data = mnist_27$train)
+ggplot(train_knn, highlight = TRUE)
+
+train_knn <- train(y ~ ., method = "knn", 
+                   data = mnist_27$train,
+                   tuneGrid = data.frame(k = seq(9, 71, 2)))
+ggplot(train_knn, highlight = TRUE)
+train_knn$bestTune
+train_knn$finalModel
+confusionMatrix(predict(train_knn, mnist_27$test, type = "raw"),
+                mnist_27$test$y)$overall["Accuracy"]
+
+control <- trainControl(method = "cv", number = 10, p = .9)
+train_knn_cv <- train(y ~ ., method = "knn", 
+                      data = mnist_27$train,
+                      tuneGrid = data.frame(k = seq(9, 71, 2)),
+                      trControl = control)
+ggplot(train_knn_cv, highlight = TRUE)
+
+train_knn$results %>% 
+  ggplot(aes(x = k, y = Accuracy)) +
+  geom_line() +
+  geom_point() +
+  geom_errorbar(aes(x = k, 
+                    ymin = Accuracy - AccuracySD,
+                    ymax = Accuracy + AccuracySD))
+
+plot_cond_prob <- function(p_hat=NULL){
+  tmp <- mnist_27$true_p
+  if(!is.null(p_hat)){
+    tmp <- mutate(tmp, p=p_hat)
+  }
+  tmp %>% ggplot(aes(x_1, x_2, z=p, fill=p)) +
+    geom_raster(show.legend = FALSE) +
+    scale_fill_gradientn(colors=c("#F8766D","white","#00BFC4")) +
+    stat_contour(breaks=c(0.5),color="black")
+}
+
+plot_cond_prob(predict(train_knn, mnist_27$true_p, type = "prob")[,2])
+
+install.packages("gam")
+modelLookup("gamLoess")
+grid <- expand.grid(span = seq(0.15, 0.65, len = 10), degree = 1)
+
+train_loess <- train(y ~ ., 
+                     method = "gamLoess",
+                     tuneGrid=grid,
+                     data = mnist_27$train)
+ggplot(train_loess, highlight = TRUE)
+confusionMatrix(data = predict(train_loess, mnist_27$test), 
+                reference = mnist_27$test$y)$overall["Accuracy"]
+
+p1 <- plot_cond_prob(predict(train_loess, mnist_27$true_p, type = "prob")[,2])
+p1
+
+set.seed(1991, sample.kind = "Rounding") # if using R 3.6 or later
+data("tissue_gene_expression")
+
+fit <- with(tissue_gene_expression, 
+            train(x, y, method = "rpart",
+                  tuneGrid = data.frame(cp = seq(0, 0.1, 0.01))))
+ggplot(fit)
+
+
+# set.seed(1991) # if using R 3.5 or earlier
+set.seed(1991, sample.kind = "Rounding") # if using R 3.6 or later
+
+fit_rpart <- with(tissue_gene_expression, 
+                  train(x, y, method = "rpart",
+                        tuneGrid = data.frame(cp = seq(0, 0.10, 0.01)),
+                        control = rpart.control(minsplit = 0)))
+
+ggplot(fit_rpart)
+confusionMatrix(fit_rpart)
+
+plot(fit_rpart$finalModel)
+text(fit_rpart$finalModel)
+
+set.seed(1991, sample.kind = "Rounding") # if using R 3.6 or later
+library(randomForest)
+fit <- with(tissue_gene_expression, 
+            train(x, y, method = "rf", 
+                  nodesize = 1,
+                  tuneGrid = data.frame(mtry = seq(50, 200, 25))))
+
+ggplot(fit)
+
+imp <- varImp(fit)
+imp
+
+tree_terms <- as.character(unique(fit_rpart$finalModel$frame$var[!(fit_rpart$finalModel$frame$var == "<leaf>")]))
+tree_terms
+
+data_frame(term = rownames(imp$importance), 
+           importance = imp$importance$Overall) %>%
+  mutate(rank = rank(-importance)) %>% arrange(desc(importance)) %>%
+  filter(term %in% tree_terms)
+
+
+
+
+
+
